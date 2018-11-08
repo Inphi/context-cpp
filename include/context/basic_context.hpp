7a7e4294b1aa97c7a69116e1eb8cb97243e4af0d
Murphy Law <mlaw2501@gmail.com>
2018-11-08 01:15:00 -0500

    document basic_context

--- include/context/basic_context.hpp
+++ include/context/basic_context.hpp
@@ -7,6 +7,13 @@
 
 namespace context {
 
+/// The main interface to context. This class can be customized via ContextImpl
+/// to provide alternative storage and lookup strategies. The ContextImpl is a
+/// template that takes in a Key type and a key comparison function. The
+/// ContextImpl must satify the following expressions that return ContextImpl:
+/// ContextImpl::make_root()` - to create the parent context.
+/// ContextImpl::make(Key, std::any, ContextImpl& parent) - to derive a new
+/// context from another context.
 template <template <class, class> class ContextImpl, class Key,
           class KeyEqual = std::equal_to<Key>>
 class basic_context {
@@ -18,13 +25,11 @@ class basic_context {
 
   basic_context(ContextImplType i) : impl_(std::move(i)) {}
 
-  /// KeyType is hashable
   basic_context with_value(Key key, std::any value) {
     auto i = ContextImplType::make(std::move(key), std::move(value), impl_);
     return basic_context{std::move(i)};
   }
 
-  /// KeyType is hashable
   template <class ValueType, class... Args>
   basic_context with_args(Key key, Args &&... args) {
     std::any value{std::in_place_type<ValueType>, std::forward<Args>(args)...};
